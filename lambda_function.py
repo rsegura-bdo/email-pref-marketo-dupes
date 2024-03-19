361122f02474522b1cad2114cc442778fca887aa
Rick Segura <rsegura@bdo.com>
2024-03-19 13:43:23 -0400

    start of lead extractions

--- lambda_function.py
+++ lambda_function.py
@@ -11,12 +11,49 @@ import logging
 import os
 
 from marketorestpython.client import MarketoClient
+from time import sleep
 
 
 
 # initialize logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+def syncDuplicates(ms:MarketoClient, email_pref:dict)->None:
+    """ 
+    Main function for synchronizing email preferefence fields of 
+    duplicate Marketo leads (by email address)
+
+    :param mc: Marketo class object
+    :param email_address: email address to filter on
+    :param field: list of fields to extract
+    :return List of dictionaries
+    """
+    
+    # build list of fields to extract
+    fields = [k for k in email_pref.keys() if k not in ('record_saved_date')]
+    logger.info(fields)
+
+
+
+def extractLeadsByEmailAddress(mc:MarketoClient, email_address:str, fields:list) -> list:
+    """ 
+    Export leads owith matching email addresses
+
+    :param mc: Marketo class object
+    :param email_address: email address to filter on
+    :param field: list of fields to extract
+    :return List of dictionaries
+    """
+
+    try:
+        leads = mc.execute(method='get_multiple_leads_by_filter_type', filterType='email', 
+                            filterValues=email_address, fields=fields)
+    except Exception as e:
+        logger.error(f'method: extractLeadsByEmailAddress()\n error: {str(e)}')
+        raise e
+    
+    return leads
 
 
 def lambda_handler(event:str, context:str):
@@ -28,6 +65,8 @@ def lambda_handler(event:str, context:str):
     :return None
     """
 
+    # sleep (5)     # sleep to give invoking call time to update updated lead 
+
     munchkin_id = os.environ["munchkin_id"]
     client_id = os.environ["client_id"]
     client_secret = os.environ["client_secret"]
@@ -37,9 +76,6 @@ def lambda_handler(event:str, context:str):
     mc = MarketoClient(munchkin_id, client_id, client_secret, api_limit, max_retry_time, requests_timeout=requests_timeout)
 
     logger.info(event)
-    logger.info(munchkin_id)
-    logger.info(client_id)
-    logger.info(client_secret)
-
-    fields = mc.execute(method='describe')
-    logger.info(fields)
+    # logger.info(munchkin_id)
+    # logger.info(client_id)
+    # logger.info(client_secret)
